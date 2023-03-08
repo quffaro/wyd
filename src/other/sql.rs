598ff65557654a8183ac4740380f30a1077abdcd
quffaro <cuffaro.matt@gmail.com>
2023-03-08 13:34:45 -0500

    working on sql.rs to be simpler

--- src/other/sql.rs
+++ src/other/sql.rs
@@ -1,9 +1,10 @@
 use crate::other::viewer::{GitConfig, Project, Status};
 use rusqlite::Connection;
 use wyd::{self, DATABASE};
+use regex::{Captures, Regex};
 
 /// CREATE TABLES
-const CREATE_CONFIG: &str = "CREATE TABLE IF NOT EXISTS tmp_git_config (path varchar(255) not null primary key, is_selected tinyint(1) default 0);";
+const CREATE_CONFIG: &str = "CREATE TABLE IF NOT EXISTS tmp_git_config (path varchar(255) not null primary key, is_selected tinyint(1) default 0, UNIQUE(path));";
 const CREATE_PROJECT: &str = "CREATE TABLE IF NOT EXISTS project (id integer primary key autoincrement, path varchar(255), name varchar(255), cat varchar(255), status varchar(255), last_commit varchar(255),);";
 const CREATE_TODO: &str = "CREATE TABLE IF NOT EXISTS todo (id integer primary key autoincrement, project_id int, todo varchar(255);";
 pub fn initialize_db() -> Result<(), rusqlite::Error> {
@@ -17,14 +18,12 @@ pub fn initialize_db() -> Result<(), rusqlite::Error> {
     Ok(())
 }
 
-
 //
 const READ_TMP: &str = "select path, is_selected from tmp_git_config where is_selected = 0";
 pub fn read_tmp() -> Result<Vec<GitConfig>, rusqlite::Error> {
     let conn = Connection::open(wyd::DATABASE)?;
 
-    let mut stmt =
-        conn.prepare(READ_TMP)?;
+    let mut stmt = conn.prepare(READ_TMP)?;
     let res = stmt
         .query_map([], |row| {
             Ok(GitConfig {
@@ -80,12 +79,9 @@ pub fn update_tmp(tmp: &GitConfig) -> Result<(), rusqlite::Error> {
 }
 
 /// WRITE TEMPORARY PROJECTS
-const CREATE_TMP: &str = "create table if not exists tmp_git_config (path varchar(255) not null primary key, is_selected tinyint(1) default 0);";
 const INSERT_INTO_TMP: &str = "INSERT INTO tmp_git_config (path) VALUES (?)";
 pub fn write_tmp(tmp: Vec<String>) -> Result<(), rusqlite::Error> {
     let conn = Connection::open(DATABASE)?;
-
-    conn.execute(CREATE_TMP, (),).expect("Failed");
 
     let mut stmt = conn.prepare(INSERT_INTO_TMP)?;
     for x in tmp {
@@ -96,23 +92,46 @@ pub fn write_tmp(tmp: Vec<String>) -> Result<(), rusqlite::Error> {
 }
 
 // all new tmps are written to
+const READ_TMP_SELECTED: &str = "select path from tmp_git_config where is_selected = 1";
 const WRITE_TMP_TO_PROJECT: &str = "insert or replace into project (path,name,cat,status) values (?1, ?2, ?3, ?4);";
 pub fn write_tmp_to_project() -> Result<(), rusqlite::Error> {
     let conn = Connection::open(DATABASE)?;
-    // match Regex::new(r#"/^(.+)\/([^\/]+)$/gm"#) {
-    //     Ok(r) => {
-    //         let re = r;
-    //     }
-    //     Err(e) => {
-    //         let path = "error.txt";
-    //         let mut output = File::create(path).unwrap();
-    //         write!(output, "{}", format!("{:?}", e))
-    //     }
-    // }
-
-    let mut stmt = conn.prepare(
+    
+    let mut read_stmt = conn.prepare(READ_TMP_SELECTED)?;
+    let mut write_stmt = conn.prepare(
         WRITE_TMP_TO_PROJECT,
     )?;
+
+    let re = Regex::new(r#"/^(.+)\/([^\/]+)$/gm"#).unwrap();
+    read_stmt
+        .query_map([], |row| {
+            // let path = row.get_unwrap(0);
+            let path = "E";
+            let caps= re.captures(path).unwrap();
+            write_stmt.execute([
+                "A".to_owned(),
+                "B".to_owned(),
+                "C".to_owned(),
+                "D".to_owned(),
+            ]).expect("WRITE ERROR");
+            Ok(())
+        }).expect("READ_ERROR")
+        .collect();
+
+    
+
+    
+
+    // for tmp in &tmp_to_write {
+    //     let caps = re.captures(&tmp).unwrap();
+    //     stmt.execute([
+    //         &tmp,
+    //         &tmp,
+    //         &"Unknown".to_owned(),
+    //         &"Unstable".to_owned(),
+    //     ]).expect("A!!!");
+    // }
+
     // for x in &tmp {
     // TODO get name of parent directory
     // let caps = re.captures(&tmp.path).unwrap();
